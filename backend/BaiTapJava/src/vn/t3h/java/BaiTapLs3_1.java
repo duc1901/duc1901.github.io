390c0d8cd03f2498b346a0e933a18c7b8f2b99c4
duc1901 <ngvanduc19@gmail.com>
2020-07-07 22:30:49 +0700

    2230

--- backend/BaiTapJava/src/vn/t3h/java/BaiTapLs3_1.java
+++ backend/BaiTapJava/src/vn/t3h/java/BaiTapLs3_1.java
@@ -1,12 +1,15 @@
 package vn.t3h.java;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class BaiTapLs3_1 {
 	public static void main(String[] args) {
 		int[] array = {90,90,20,27,35,22,10,8,8};
 		System.out.println("a) ");
 		insertElement(array);
+		System.out.println("b) ");
+		removeDuplicateElement(array);
 	}
 	
 	public static void insertElement(int[] array) {
@@ -16,5 +19,13 @@ public class BaiTapLs3_1 {
 		}
 		array[position] = 100;
 		System.out.println("new array: " + Arrays.toString(array));
+	}
+	
+	public static void removeDuplicateElement(int[] array) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int num : array) {
+		    set.add(num);
+		}
+		System.out.println(set);
 	}
 }
